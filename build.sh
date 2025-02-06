dc219cf6f447c9980602ad9981358c577bf8d1ed
Nixxa <52290162+sogeking-pixel@users.noreply.github.com>
2025-02-06 14:11:36 -0500

    xd xd sds

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -o errexit
 pip install -r requirements.txt
 
 # Convert static asset files
-python manage.py collectstatic
+python manage.py collectstatic --noinput
 
 # Apply any outstanding database migrations
 python manage.py migrate
